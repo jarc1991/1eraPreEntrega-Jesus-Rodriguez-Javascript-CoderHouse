a55446ff793f0d1fbe5b9d16481b78824507c2c1
jarc1991 <jesuscontell2020@gmail.com>
2023-03-21 23:24:54 -0300

    Eliminando archivos y realizando arreglos a archivo JS

--- js/script.js
+++ js/script.js
@@ -37,34 +37,100 @@ if ((isNaN (contrasenia)) || contrasenia == " ") {
 
 let usuarioIngresado = prompt ("Ingrese su usuario registrado") .toUpperCase()
 let contraseniaIngresada = Number (prompt("Ingrese la contraseña registrada"))
-
-for (i = 0; i < 2; i++){
-
-if (usuarioIngresado === nombreUsuario && contraseniaIngresada === contrasenia && isNaN (usuarioIngresado) && usuarioIngresado != " ") {
-
-    alert (bienvenida + " " + usuarioIngresado)
-    break
-
-}
-
-else if (usuarioIngresado !== nombreUsuario && contraseniaIngresada !== contrasenia && contador <= 3 || usuarioIngresado == " " || isNaN (contraseniaIngresada)) {
-
-    alert ("Debe ingresar el usuario y contraseña registrados.")
-    usuarioIngresado = prompt ("Ingrese su usuario registrado")
-    contraseniaIngresada = Number (prompt("Ingrese la contraseña registrada"))
-    
-}
-
-else {
-
-    alert ("Intente nuevamente más tarde")
-} }
-
 let menu = "1- Saber un poco de Adele.\n2- Discos de Adele.\n3- Precios Discos. \n4- Compra. \n5- Compra en cuotas.\n6- Salir."
 let opcion = 0
 let compra = 50
 
-// Función
+validarUsuario ()
+
+// Funciones
+
+function validarUsuario() {
+
+    for (i = 0; i < 2; i++){
+
+    if (usuarioIngresado === nombreUsuario && contraseniaIngresada === contrasenia && isNaN (usuarioIngresado) && usuarioIngresado != " ") {
+
+        alert (bienvenida + " " + usuarioIngresado)
+
+        mostrarMenu()
+
+        break
+
+        }
+
+    else if (usuarioIngresado !== nombreUsuario && contraseniaIngresada !== contrasenia && contador <= 3 || usuarioIngresado == " " || isNaN (contraseniaIngresada)) {
+
+        alert ("Debe ingresar el usuario y contraseña registrados.")
+        usuarioIngresado = prompt ("Ingrese su usuario registrado")
+        contraseniaIngresada = Number (prompt("Ingrese la contraseña registrada"))
+        
+        }
+
+    else {
+
+        alert ("Intente nuevamente más tarde")
+        } 
+    }
+
+}
+
+function mostrarMenu() {
+
+    do {
+    
+        alert (menu)
+    
+        opcion = Number (prompt ("Ingrese la opción: "))
+        
+        if (opcion === 1) {
+    
+            bioAdele ()
+        }
+    
+        else if (opcion === 2) {
+    
+            mostrarDiscos()
+        }
+    
+        else if (opcion === 3) {
+    
+            let selectDisc = Number (prompt ("¿Cual cd quieres comprar, debes indicar 19 o 21 o 25 o 30? También pues introducir 0 para salir"))
+
+            if (selectDisc === 19 || selectDisc === 21 || selectDisc === 25 || selectDisc === 30) {
+
+            precioDiscos() 
+            
+            }
+
+            else if (selectDisc == "0") {
+        
+                alert ("Volver")
+            }
+            
+            else {
+            
+                alert ("Debes introducir una opción.") 
+            
+            }
+        } 
+    
+        else if (opcion === 4) {
+    
+            comprarDiscos()
+    
+        }
+    
+        else if (opcion === 5) {
+    
+           comprasInteres()
+    
+        }
+    }
+    
+    while (opcion != 6) 
+    
+    }
 
 function precioDiscos () {
 
@@ -72,27 +138,58 @@ function precioDiscos () {
 
 }
 
-do {
+function bioAdele() {
 
-    alert (menu)
+    alert ("Adele Laurie Blue Adkins (Londres, 5 de mayo de 1988), conocida simplemente como Adele, es una cantautora y multinstrumentista británica. Es una de las artistas musicales con mayores ventas del mundo, con más de 120 millones de ventas entre discos y sencillo.")
+}
 
-    opcion = Number (prompt ("Ingrese la opción: "))
+function mostrarDiscos() {
+
+    alert ("Los discos de Adele son: 19, 21, 25, 30.")
+}
+
+function comprarDiscos() {
+
+    let numDisc = Number (prompt("¿Cuántos discos desea comprar?"))
     
-    if (opcion === 1) {
+            for (i = 0; i < numDisc; i++) {
+            
+            let compraDisc = Number (prompt ("Introduce el cd que desea comprar ejemplo: 19, 21, 25 o 30"))
+            
+            alert ("Tiene un valor de " + " $ " + compra)
+        
+        }
+    
+        alert ("Tu total a pagar es : " + (numDisc * compra))
 
-        alert ("Adele Laurie Blue Adkins (Londres, 5 de mayo de 1988), conocida simplemente como Adele, es una cantautora y multinstrumentista británica. Es una de las artistas musicales con mayores ventas del mundo, con más de 120 millones de ventas entre discos y sencillo.")
-    }
+        mostrarMenu()
 
-    else if (opcion === 2) {
+}
 
-        alert ("Los discos de Adele son: 19, 21, 25, 30.")
-    }
+function comprasInteres() {
 
-    else if (opcion === 3) {
+    numDisc = Number (prompt("¿Cuántos discos desea comprar?"))
 
-        let selectDisc = prompt ("¿Cual cd quieres comprar, debes indicar 19 o 21 o 25 o 30? También pues introducir 0 para salir")
+    let cantCuotas = Number (prompt ("Diga en cuántas cuotas (sólo se permiten hasta 12). ADVERTENCIA: RECARGO DEL 25% anual"))
+    let interesCuota = Number (25/100)/12
+    
+    let calCuotas = ((((numDisc * compra) * interesCuota) * cantCuotas) + (numDisc * compra)) .toFixed(2)
 
-        switch (selectDisc) {
+    alert ("Su total a pagar en: " + cantCuotas + " " + "cuotas es de: " + calCuotas)
+}
+
+
+
+
+
+
+
+
+//Otra forma para hacer la opción 3 del menú*/
+
+
+
+        /*switch (selectDisc) {
 
             case "19": precioDiscos()
 
@@ -120,75 +217,4 @@ do {
 
             break
 
-        }
-    
-    } 
-
-    else if (opcion === 4) {
-
-        let numDisc = Number (prompt("¿Cuántos discos desea comprar?"))
-
-        for (i = 0; i < numDisc; i++) {
-        
-        let compraDisc = Number (prompt ("Introduce el cd que desea comprar ejemplo: 19, 21, 25 o 30"))
-        
-        alert ("Tiene un valor de " + " $ " + compra)
-    
-    }
-
-    alert ("Tu total a pagar es : " + (numDisc * compra))
-
-    }
-
-    else if (opcion === 5) {
-
-        numDisc = Number (prompt("¿Cuántos discos desea comprar?"))
-
-        let cantCuotas = Number (prompt ("Diga en cuántas cuotas (sólo se permiten hasta 12). ADVERTENCIA: RECARGO DEL 25% anual"))
-        let interesCuota = Number (25/100)/12
-        
-        let calCuotas = ((((numDisc * compra) * interesCuota) * cantCuotas) + (numDisc * compra)) .toFixed(2)
-
-        alert ("Su total a pagar en: " + cantCuotas + " " + "cuotas es de: " + calCuotas)
-
-    }
-}
-
-while (opcion != 6) 
-
-
-
-
-
-
-
-//Otra forma para hacer la opción 3 del menú
-
-    /*if (selectDisc == "19") {
-
-            precioDiscos ()
-        }
-        
-        else if (selectDisc == "21") {
-        
-            precioDiscos ()
-        }
-        
-        else if (selectDisc == "25") {
-        
-            precioDiscos ()
-        }
-        
-        else if (selectDisc == "30") {
-        
-            precioDiscos ()
-        }
-        
-        else if (selectDisc == "0") {
-        
-            alert ("Volver")
-        }
-        
-        else {
-        
-            alert ("Debes introducir una opción.")*/
+    }*/      
